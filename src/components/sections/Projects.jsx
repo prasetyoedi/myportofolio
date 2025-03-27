7000c010b7de11eedc341ac934faf8fe64483f18
prasetyoedi <prasetyoedi451@gmail.com>
2025-03-27 12:02:23 +0700

    add Projects component to showcase featured projects with descriptions and technologies used

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,163 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> CalMind - Mental Health Application</h3>
+              <p className="text-gray-400 mb-4">
+                Together with the amazing Group 6 (Alterra Academy Capstone Project), we developed CalMind, an innovative mental health application designed to support users, doctors, and administrators effectively.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "TailwindCSS", "Axios", "Preline UI", "Golang", "Gemini AI", "Midtrans"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Kelompok-6-capstone/Front-End"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Eco Shop</h3>
+              <p className="text-gray-400 mb-4">
+                A retail store that focuses on selling eco-friendly and sustainable products. It aims to promote a greener lifestyle by providing environmentally conscious alternatives for everyday needs.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "TailwindCSS", "Supabase", "Sweetalert", "Chatbot AI Gemini"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/prasetyoedi/ecoShop"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Denteksi/Senyumin</h3>
+              <p className="text-gray-400 mb-4">
+                A web-based application designed for early detection of dental caries for children. It utilizes AI-powered image analysis to assess dental conditions and determine whether further examination is needed. The platform aims to assist parents  in monitoring children's oral health more effectively.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Laravel 10", "Bootstrap 5", "Cropper.jS", "Machine Learning"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Haikal-Adityo/Cinurawa"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Gelanggang Expo UGM 2023</h3>
+              <p className="text-gray-400 mb-4">
+                An annual event at Universitas Gadjah Mada aimed at introducing various student activity units (UKM) and communities to new students. The event provided information about organizations, showcased performances, and encouraged student engagement in extracurricular activities to develop skills and networks beyond academics.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
